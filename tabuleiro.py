e1b786b4d5a3b22d2f5cc74a83278c04a747021c
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2022-06-09 09:37:18 -0300

    Refatoração dos códigos de cores de tabuleiro para o arquivo def_cores.

--- tabuleiro.py
+++ tabuleiro.py
@@ -13,11 +13,11 @@ from def_cores import *
 def print_tabuleiro():
     """Definição da função que printa o tabuleiro e as peças na tela"""
 
-    print("\u001b[37;1mPeças Escuras retiradas do jogo: \u001b[48;5;244m", end = '')
+    print(COR_BRANCO_BRILHANTE + "Peças Escuras retiradas do jogo: " + COR_FUNDO_CINZA, end = '')
     for cont in range(15):
         print(var_global.pecas_retiradas_escuras[cont], end = ' ')
     print(COR_RESETALL)
-    print("\u001b[37;1mPeças Claras retiradas do jogo: \u001b[48;5;244m", end = '')
+    print(COR_BRANCO_BRILHANTE + "Peças Claras retiradas do jogo: " + COR_FUNDO_CINZA, end = '')
     for cont in range(15):
         print(var_global.pecas_retiradas_claras[cont], end = ' ')
     print(COR_RESETALL + "\n")
@@ -29,19 +29,19 @@ def print_tabuleiro():
         # Lado esquerdo
         for coluna in range(6):
             if coluna % 2 == 0:
-                print(var_global.CS_CLARA + "\u001b[48;5;12m " + var_global.pecas_posi[coluna][linha] + " " + COR_RESETALL + var_global.CS_CLARA, end = '')
+                print(var_global.CS_CLARA + COR_FUNDO_AZUL_CLARO + " " + var_global.pecas_posi[coluna][linha] + " " + COR_RESETALL + var_global.CS_CLARA, end = '')
             else:
-                print(var_global.CS_ESCURA + "\u001b[48;5;9m " + var_global.pecas_posi[coluna][linha] + " " + COR_RESETALL + var_global.CS_ESCURA, end = '')
+                print(var_global.CS_ESCURA + COR_FUNDO_VERM_CLARO + " " + var_global.pecas_posi[coluna][linha] + " " + COR_RESETALL + var_global.CS_ESCURA, end = '')
         
         # Meio do tabuleiro para as peças claras capturadas
-        print(var_global.CS_MEIO + "\u001b[42;1m " + var_global.pecas_capturadas_claras[linha] + " " + COR_RESETALL + var_global.CS_MEIO, end = '')
+        print(var_global.CS_MEIO + COR_FUNDO_VERDE_BRIL + " " + var_global.pecas_capturadas_claras[linha] + " " + COR_RESETALL + var_global.CS_MEIO, end = '')
         
         # Lado direito
         for coluna in range(6, 12):
             if coluna % 2 == 0:
-                print(var_global.CS_CLARA + "\u001b[48;5;12m " + var_global.pecas_posi[coluna][linha] + " " + COR_RESETALL + var_global.CS_CLARA, end = '')
+                print(var_global.CS_CLARA + COR_FUNDO_AZUL_CLARO + " " + var_global.pecas_posi[coluna][linha] + " " + COR_RESETALL + var_global.CS_CLARA, end = '')
             else:
-                print(var_global.CS_ESCURA + "\u001b[48;5;9m " + var_global.pecas_posi[coluna][linha] + " " + COR_RESETALL + var_global.CS_ESCURA, end = '')
+                print(var_global.CS_ESCURA + COR_FUNDO_VERM_CLARO + " " + var_global.pecas_posi[coluna][linha] + " " + COR_RESETALL + var_global.CS_ESCURA, end = '')
         print()
     print()
 
@@ -51,18 +51,18 @@ def print_tabuleiro():
         # Lado esquerdo
         for coluna in range(23, 17, -1):
             if coluna % 2 == 0:
-                print(var_global.CS_CLARA + "\u001b[48;5;12m " + var_global.pecas_posi[coluna][linha-1] + " " + COR_RESETALL + var_global.CS_CLARA, end = '')
+                print(var_global.CS_CLARA + COR_FUNDO_AZUL_CLARO + " " + var_global.pecas_posi[coluna][linha-1] + " " + COR_RESETALL + var_global.CS_CLARA, end = '')
             else:
-                print(var_global.CS_ESCURA + "\u001b[48;5;9m " + var_global.pecas_posi[coluna][linha-1] + " " + COR_RESETALL + var_global.CS_ESCURA, end = '')
+                print(var_global.CS_ESCURA + COR_FUNDO_VERM_CLARO + " " + var_global.pecas_posi[coluna][linha-1] + " " + COR_RESETALL + var_global.CS_ESCURA, end = '')
         
         # Meio do tabuleiro para as peças escuras capturadas
-        print(var_global.CS_MEIO + "\u001b[42;1m " + var_global.pecas_capturadas_escuras[linha-1] + " " + COR_RESETALL + var_global.CS_MEIO, end = '')
+        print(var_global.CS_MEIO + COR_FUNDO_VERDE_BRIL + " " + var_global.pecas_capturadas_escuras[linha-1] + " " + COR_RESETALL + var_global.CS_MEIO, end = '')
         
         # Lado direito
         for coluna in range(17, 11, -1):
             if coluna % 2 == 0:
-                print(var_global.CS_CLARA + "\u001b[48;5;12m " + var_global.pecas_posi[coluna][linha-1] + " " + COR_RESETALL + var_global.CS_CLARA, end = '')
+                print(var_global.CS_CLARA + COR_FUNDO_AZUL_CLARO + " " + var_global.pecas_posi[coluna][linha-1] + " " + COR_RESETALL + var_global.CS_CLARA, end = '')
             else:
-                print(var_global.CS_ESCURA + "\u001b[48;5;9m " + var_global.pecas_posi[coluna][linha-1] + " " + COR_RESETALL + var_global.CS_ESCURA, end = '')
+                print(var_global.CS_ESCURA + COR_FUNDO_VERM_CLARO + " " + var_global.pecas_posi[coluna][linha-1] + " " + COR_RESETALL + var_global.CS_ESCURA, end = '')
         print()
     print(COR_NEGRITO + "     01   02   03   04   05   06        07   08   09   10   11   12" + COR_RESETALL + "\n")
