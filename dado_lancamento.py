6f415b3ccc6b59916e21c4114a213c193514aeb0
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2022-06-09 11:38:24 -0300

    Pequeno refatoramento de rola_dado

--- dado_lancamento.py
+++ dado_lancamento.py
@@ -66,12 +66,8 @@ def print_dado():
 def rola_dado(jogador):
 
     """Função que retorna, para o respectivo jogador, os valores de dados lançados para a jogada"""
-    if var_global.jog_1:
-        print(jogador + ", aperte enter para rolar os dados:")
-        input()
-    else:
-        print(jogador + ", aperte enter para rolar os dados:")
-        input()
+    print(jogador + ", aperte enter para rolar os dados:")
+    input()
     
     var_global.dado_1 = random.randint(1, 6)
     var_global.dado_2 = random.randint(1, 6)
